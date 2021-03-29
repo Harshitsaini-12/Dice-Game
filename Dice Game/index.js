5ff8d5e4435df191367896577d3a8bf3926ac507
Someone-codes-harshit <81574700+Someone-codes-harshit@users.noreply.github.com>
2021-03-30 01:25:06 +0530

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+
+var randomDiceImage ="dice"+ randomNumber1 +".png";
+
+var randomImageSource ="images/"+ randomDiceImage;
+
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+
+
+
+// 2 dice 
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomImageSource_2 ="images/dice"+ randomNumber2 + ".png";
+
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource_2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins!  🚩"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML ="Player 2 wins!  🚩"
+}
+
+else{
+    document.querySelector("h1").innerHTML="Draw."
+}
